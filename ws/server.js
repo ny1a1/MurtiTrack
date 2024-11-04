4acbddbf8efb99f30e5de6a03a7ce8c64713852c
ny1a1 <n.yakush@istu.edu.ua>
2024-11-04 16:47:31 +0200

    task-6

--- ws/server.js
+++ ws/server.js
@@ -1,23 +1,5 @@
-const { WebSocketServer, WebSocket } = require('ws');
+const { ChatServer } = require('./server/ChatServer');
 
-const wss = new WebSocketServer({ port: 8080 });
+const chatServer = new ChatServer({ port: 8080 });
 
-const clients = [];
-
-wss.on('connection', function connection(ws) {
-    console.log("new connection");
-    const id = clients.length;
-    clients.push(ws);
-
-  ws.on('message', function message(data) {
-    console.log('received msg from client: %s', data);
-
-    wss.clients.forEach(function forEach(client) {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(data);
-        }
-    });
-  });
-
-  ws.on('error', console.error);
-});
+chatServer.init();
